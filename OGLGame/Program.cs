3b426a54b5e758d19dbcc36b9a0b86a85f6a8451
Legion-143 <Legion-143@DESKTOP-LV6298J>
2023-09-10 16:24:06 +0300

    Add project files.

---
+++ OGLGame/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace OGLGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}
